6af2b41a08cc20163245292d4edf93f1df557eb2
zhengyongkai <41411396+zhengyongkai@users.noreply.github.com>
2021-01-06 19:54:34 +0800

    vue3

---
+++ src/views/login/login.vue
@@ -0,0 +1,24 @@
+<!--  -->
+<template>
+  <div>
+      hello vue3
+  </div>
+</template>
+<script >
+export default {
+    data() {
+      return{
+      }
+    },
+    props:[],
+    components: {},
+    created() {
+    },
+    mounted() {
+    },
+    methods:{
+    }
+  };
+</script>
+<style lang='scss' scoped>
+</style>
